41b2c041098f5410aaf5e7e94390879d978be6a7
Micah Tidball <micahtid@gmail.com>
2024-12-21 21:14:39 +0700

    completed portfolio!

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -22,21 +22,13 @@ const Home = () => {
       <p
       className='
       dynamic-text'>
-      Your friendly neighborhood frontend developer, 
-      UX architect, and JavaScript engineer. I spend my days (and often nights) 
-      painting the Internet canvas with Projects and lines of code, turning zeroes 
-      and ones into immersive, interactive experiences,
-      </p>
-
-      <p
-      className='
-      dynamic-text'>
-      Bona fide photochromic Lens enthusiast - sunlight or indoors, 
-      I&apos;ve got it covered. I tread the path of minimalism, finding 
-      beauty in simplicity and order. When I&apos;m not crafting beautiful 
-      web experiences, you can find me reading Articles or swaying to 
-      the rhythm of Pop Music & Jazz, losing myself in the captivating 
-      flow of melodies. anyways you can Contact Me
+      Just your neighborhood full-stack developer! Specializing in <span className="font-medium">React, TailwindCSS, Next.js, and Firebase</span>, 
+      my 8:00 PM staple has become creating interactive web canvases <span className="font-medium">inspired by the little things around me.</span>
+      <br /> <br />
+      I&apos;m <span className="font-medium">experienced in backend architecture and database management</span> but have a soft spot for minimalist, 
+      clean front-end designs—there&apos;s just something special about easy-to-navigate web apps! When I&apos;m not coding, you&apos;ll 
+      find me swimming, training calisthenics, dabbling in robotics <span className="font-medium">(a proud STEM enthusiast!)</span>, or reading the Bible. 
+      Feel free to check out the rest of my portfolio!
       </p>
       
       <ArrowText text="See More About Me" link="/about" className="mt-8" />
