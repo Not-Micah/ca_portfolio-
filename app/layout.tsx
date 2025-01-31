24cc594e15d13efec849d4fa6e76305557b1ae7c
Micah Tidball <micahtid@gmail.com>
2025-01-31 14:18:09 +0700

    enabled Vercel analytics!

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { Analytics } from '@vercel/analytics/next';
+
 import Backdrop from "./components/Backdrop";
 import Cursor from "./components/Cursor";
 
@@ -36,6 +38,7 @@ export default function RootLayout({
         <Backdrop />
         <Cursor />
         {children}
+        <Analytics />
       </body>
     </html>
   );
